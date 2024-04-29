10543f913dc2105afa86e6ee2e1f2649f1f09dcc
Korbielowski <dawidstasinski12@gmail.com>
2024-04-29 18:02:02 +0200

    Added Poll model and UserPollAnswer model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 from .models import CloserUser, Friendship
-from posts.models import Post
+from posts.models import Post, Poll, UserPollAnswer
 
-admin.site.register((CloserUser, Friendship, Post))
+admin.site.register((CloserUser, Friendship, Post, Poll, UserPollAnswer))
--- posts/models.py
+++ posts/models.py
@@ -12,3 +12,28 @@ class Post(models.Model):
     title = models.CharField(max_length=100, default="")
     description = models.CharField(max_length=500)
     image = models.ImageField(upload_to="posts_images/", blank=True)
+
+
+class Poll(models.Model):
+    author = models.ForeignKey(
+        CloserUser, default=None, on_delete=models.CASCADE, related_name="author"
+    )
+    creation_date = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=100, default="")
+    question = models.CharField(max_length=500)
+    first_ans = models.CharField(max_length=500)
+    second_ans = models.CharField(max_length=500)
+    third_and = models.CharField(max_length=500)
+    fourth_ans = models.CharField(max_length=500)
+    votes = models.IntegerField(default=0)
+
+
+class UserPollAnswer(models.Model):
+    poll = user = models.ForeignKey(
+        Poll, default=None, on_delete=models.CASCADE, related_name="poll"
+    )
+    user = models.ForeignKey(
+        CloserUser, default=None, on_delete=models.CASCADE, related_name="respondent"
+    )
+    answer = models.CharField(max_length=500)
+    answer_date = models.DateField(auto_now_add=True)
